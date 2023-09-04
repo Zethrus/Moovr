b626e2000f64e3e7f3bdacb03b9b60e38904d783
Zethrus <contact@zethr.us>
2023-09-04 10:13:25 -0600

    er

--- src/main/java/us/zethr/us/moovr/Moovr.java
+++ src/main/java/us/zethr/us/moovr/Moovr.java
@@ -9,24 +9,24 @@ import org.bukkit.plugin.java.JavaPlugin;
 //import org.mcstats.MetricsLite;
 
 public class Moovr extends JavaPlugin implements Listener {
-  public Player sender;
-  
-  public void onEnable() {
-    // Create an instance of MoovrBlock
-    MoovrBlock moovrBlock = new MoovrBlock(this);
+    public Player sender;
 
-    PluginManager pluginManager = getServer().getPluginManager();
-    pluginManager.registerEvents(moovrBlock, this);
-    saveDefaultConfig();
-    //try {
-      //MetricsLite metrics = new MetricsLite((Plugin)this);
-      //metrics.start();
-    //} catch (IOException e) {
-      //System.out.println("PluginMetrics: He's dead, Jim!");
-    //} 
-  }
-  
-  public void onDisable() {
-    
-  }
+    public void onEnable() {
+        // Create an instance of MoovrBlock
+        MoovrBlock moovrBlock = new MoovrBlock(this);
+
+        PluginManager pluginManager = getServer().getPluginManager();
+        pluginManager.registerEvents(moovrBlock, this);
+        saveDefaultConfig();
+        //try {
+        //MetricsLite metrics = new MetricsLite((Plugin)this);
+        //metrics.start();
+        //} catch (IOException e) {
+        //System.out.println("PluginMetrics: He's dead, Jim!");
+        //} 
+    }
+
+    public void onDisable() {
+
+    }
 }
