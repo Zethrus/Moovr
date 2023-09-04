8992586be9d9bd83138894b93a340b080ebf0c3e
Zethrus <contact@zethr.us>
2023-09-04 14:11:49 -0600

    teleport player instead?

--- src/main/java/us/zethr/us/moovr/Moovr.java
+++ src/main/java/us/zethr/us/moovr/Moovr.java
@@ -15,7 +15,7 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
-import org.bukkit.Location;
+import
 
 public class Moovr extends JavaPlugin implements Listener {
   private double moovrSpeed; // Store Moovr speed into variable
@@ -49,30 +49,30 @@ public class Moovr extends JavaPlugin implements Listener {
 
   }
 
-  @EventHandler
-  public void onPlayerMoveEvent(PlayerMoveEvent event) {
+@EventHandler
+public void onPlayerMoveEvent(PlayerMoveEvent event) {
     Player player = event.getPlayer();
     Block block = player.getLocation().getBlock();
     Block blockAbove = block.getRelative(BlockFace.UP);
 
     if ((player.hasPermission("moovr.use") || player.isOp()) && blockAbove.getType() == Material.AIR) {
-      if (block.getType() == Material.POWERED_RAIL) {
-        Block blockUnder = block.getRelative(BlockFace.DOWN);
-        if (blockUnder.getType() == Material.GOLD_BLOCK
-            && blockUnder.getRelative(BlockFace.DOWN).getType() == Material.REDSTONE_TORCH) {
-          float walkSpeed = (float) Math.max(Math.min(moovrSpeed, 1.0), -1.0);
-          player.setWalkSpeed(walkSpeed);
+        if (block.getType() == Material.POWERED_RAIL) {
+            Block blockUnder = block.getRelative(BlockFace.DOWN);
+            if (blockUnder.getType() == Material.GOLD_BLOCK && blockUnder.getRelative(BlockFace.DOWN).getType() == Material.REDSTONE_TORCH) {
+                float walkSpeed = (float) Math.max(Math.min(moovrSpeed, 1.0), -1.0);
+                player.setWalkSpeed(walkSpeed);
 
-          Location playerLocation = player.getLocation();
-          Vector direction = playerLocation.getDirection().normalize();
-          player.setVelocity(direction.multiply(0.5));
+                Location playerLocation = player.getLocation();
+                Vector direction = playerLocation.getDirection().normalize();
+                playerLocation.add(direction.multiply(0.5));
+                player.teleport(playerLocation);
+            }
+        } else {
+            float defaultWalkSpeed = 0.2F;
+            player.setWalkSpeed(defaultWalkSpeed);
         }
-      } else {
-        float defaultWalkSpeed = 0.2F;
-        player.setWalkSpeed(defaultWalkSpeed);
-      }
     }
-  }
+}
 
   @Override
   public void onDisable() {
