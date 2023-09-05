4af02f44207fed9058262153aea63d05450dc467
Zethrus <contact@zethr.us>
2023-09-04 18:53:19 -0600

    Remove automatic walk

--- src/main/java/us/zethr/us/moovr/MoovrPlayerMoveListener.java
+++ src/main/java/us/zethr/us/moovr/MoovrPlayerMoveListener.java
@@ -1,14 +1,12 @@
 package us.zethr.us.moovr;
 
 import org.bukkit.Material;
-import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.util.Vector;
 
 public class MoovrPlayerMoveListener implements Listener {
 
@@ -30,11 +28,6 @@ public class MoovrPlayerMoveListener implements Listener {
                 if (blockUnder.getType() == Material.GOLD_BLOCK && blockUnder.getRelative(BlockFace.DOWN).getType() == Material.REDSTONE_TORCH) {
                     float walkSpeed = (float) Math.max(Math.min(plugin.getMoovrSpeed(), 1.0), -1.0);
                     player.setWalkSpeed(walkSpeed);
-
-                    Location playerLocation = player.getLocation();
-                    Vector direction = playerLocation.getDirection().normalize();
-                    playerLocation.add(direction.multiply(0.5));
-                    player.teleport(playerLocation);
                 }
             } else {
                 float defaultWalkSpeed = 0.2F;
