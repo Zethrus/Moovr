35ebc3426fd87d60ff2314bee589ef5845ca8ce1
Zethrus <contact@zethr.us>
2023-09-04 20:20:55 -0600

    should reload

--- src/main/java/us/zethr/us/moovr/MoovrReloadCommand.java
+++ src/main/java/us/zethr/us/moovr/MoovrReloadCommand.java
@@ -1,5 +1,6 @@
 package us.zethr.us.moovr;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -14,18 +15,15 @@ public class MoovrReloadCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (command.getName().equalsIgnoreCase("moovr") && args.length > 0 && args[0].equalsIgnoreCase("reload")) {
-            if (sender.hasPermission("moovr.reload")) {
-                plugin.reloadConfig();
-                plugin.loadConfig(); // Add this line to update the moovrSpeed variable
-                sender.sendMessage("Moovr configuration reloaded.");
-                plugin.getLogger().info("Moovr configuration reloaded."); // Add debug message
-                return true;
-            } else {
-                sender.sendMessage("You do not have permission to reload the Moovr configuration.");
-                return true;
-            }
+        if (!sender.hasPermission("moovr.reload")) {
+            sender.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
+            return true;
         }
-        return false;
+
+        plugin.reloadConfig();
+        plugin.loadConfig();
+        sender.sendMessage(ChatColor.GREEN + "Moovr configuration reloaded.");
+
+        return true;
     }
 }
